e0ee2d8c419da76c14b0193d0b5219fb7921152c
Unc0ver001 <73369813+Unc0ver001@users.noreply.github.com>
2021-01-08 20:02:07 +0800

    Update BaiduCloud.js

--- Json/BaiduCloud.js
+++ Json/BaiduCloud.js
@@ -1,7 +1,7 @@
 /*
 Baidu netdisc unlocks online video play speed.
 Surge4.0:
-http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Unc0ver001/Unc0ver/main/Json/BaiduCloud.js
+BaiduCloudVIP = requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Unc0ver001/Unc0ver/main/Json/BaiduCloud.js,type=http-response,pattern=https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user
 
 QX1.0.0:
 https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/Unc0ver001/Unc0ver/main/Json/BaiduCloud.js
