3352d87b62519c1cc3ed40061d0edac436ddee46
Unc0ver001 <73369813+Unc0ver001@users.noreply.github.com>
2021-01-08 19:29:39 +0800

    Update BaiduCloud.js

--- Json/BaiduCloud.js
+++ Json/BaiduCloud.js
@@ -2,10 +2,10 @@
 Baidu netdisc unlocks online video play speed.
 Key data from thor filter
 Surge4.0:
-http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Unc0ver001/Unc0ver/main/Json/BaiduCloud.js
 
 QX1.0.0:
-https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/Unc0ver001/Unc0ver/main/Json/BaiduCloud.js
 
 MITM = pan.baidu.com
 */
@@ -15,7 +15,7 @@ obj = {
   "product_infos": [{
     "product_id": "5310897792128633390",
     "start_time": 1417260485,
-    "end_time": 2147483648,
+    "end_time": 2222222222,
     "buy_time": "1417260485",
     "cluster": "offlinedl",
     "detail_cluster": "offlinedl",
@@ -24,12 +24,12 @@ obj = {
     "product_name": "svip2_nd",
     "product_description": "超级会员",
     "function_num": 0,
-    "start_time": 1553702399,
+    "start_time": 1417260485,
     "buy_description": "",
-    "buy_time": 0,
+    "buy_time": "1417260485",
     "product_id": "1",
     "auto_upgrade_to_svip": 0,
-    "end_time": 1672502399,
+    "end_time": 2222222222,
     "cluster": "vip",
     "detail_cluster": "svip",
     "status": 0
