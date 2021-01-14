082217ec00e49060c6fbac366f8c5cf8dd78b75a
Unc0ver001 <73369813+Unc0ver001@users.noreply.github.com>
2021-01-14 18:04:02 +0800

    Add files via upload

---
+++ Json/vxad.js
@@ -0,0 +1,9 @@
+﻿/*
+MITM = mp.weixin.qq.com,
+*/
+
+var obj = JSON.parse($response.body);
+obj.advertisement_num = 0;
+obj.advertisement_info = [];
+delete obj.appid;
+$done({body: JSON.stringify(obj)}); 
