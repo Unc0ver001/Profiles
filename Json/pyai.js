c54e1b396e8361de9f9e66d40b4c9953770baabe
Unc0ver001 <73369813+Unc0ver001@users.noreply.github.com>
2021-02-20 21:29:12 +0800

    Add files via upload

---
+++ Json/pyai.js
@@ -0,0 +1,16 @@
+﻿var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const path1 = '/user.htm';
+const path2 = '/book.htm';
+if (url.indexOf(path1) != -1) {
+	obj.data["vip"] = "1";
+	obj.data["nickName"] = "999";
+	body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(path2) != -1) {
+var body = $response.body.replace(/"isFree":"0"/g, '"isFree":"1"');
+ }
+$done({body});
